bfade451fb6b5eeb1518d04cbfcf1f816ced82a7
Hennadii Bublikov <99074724+HennadiiBu@users.noreply.github.com>
2023-08-09 21:30:36 +0300

    add lazy loading

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,26 @@
 import { Route, Routes } from 'react-router-dom';
-import Layuot from './Layuot';
-import Tranding from 'pages/Tranding';
-import MoviesPage from 'pages/MoviesPage';
-import Review from './Review/Review';
-import Cast from './Cast/Cast';
-import MoviesDatailesPage from 'pages/MoviesDatailesPage';
+import { Suspense, lazy } from 'react';
+
+const Layuot = lazy(() => import('./Layuot'));
+const Tranding = lazy(() => import('pages/Tranding'));
+const MoviesPage = lazy(() => import('pages/MoviesPage'));
+const Review = lazy(() => import('./Review/Review'));
+const Cast = lazy(() => import('./Cast/Cast'));
+const MoviesDatailesPage = lazy(() => import('../pages/MoviesDatailesPage'));
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Layuot />}>
-        <Route index element={<Tranding />} />
-        <Route path="movies" element={<MoviesPage />} />
-        <Route path="movies/:movieId" element={<MoviesDatailesPage />}>
-          <Route path="review" element={<Review />} />
-          <Route path="cast" element={<Cast />} />
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<Layuot />}>
+          <Route index element={<Tranding />} />
+          <Route path="movies" element={<MoviesPage />} />
+          <Route path="movies/:movieId" element={<MoviesDatailesPage />}>
+            <Route path="review" element={<Review />} />
+            <Route path="cast" element={<Cast />} />
+          </Route>
         </Route>
-      </Route>
-    </Routes>
+      </Routes>
+    </Suspense>
   );
 };
